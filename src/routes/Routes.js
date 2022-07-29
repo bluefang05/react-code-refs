ada114928c161988ca1d93b635f0d67148e452e5
Enmanuel Domínguez <manuelx_05@hotmail.com>
2022-07-29 16:26:36 -0400

    SCREAMING_SNAKE_CASE

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Routes as RouteList, Route } from "react-router-dom";
 import ConditionalRender1 from '../containers/Conditional/1/ConditionalRender';
 import ConditionalDisable1 from '../containers/Conditional/2/ConditionalDisable1';
-import ConditionalDisable1_2 from '../containers/Conditional/2-2/ConditionalDisable1_2';
+import CONDITIONAL_DISABLE_1_2 from '../containers/Conditional/2-2/ConditionalDisable1_2';
 function Routes() {
   return (
     <RouteList>
         <Route path="/conditional-render-1" element={<ConditionalRender1/>} />
         <Route path="/conditional-disable-1" element={<ConditionalDisable1/>} />
-        <Route path="/conditional-disable-1-2" element={<ConditionalDisable1_2/>} />
+        <Route path="/conditional-disable-1-2" element={<CONDITIONAL_DISABLE_1_2/>} />
     </RouteList>
   )
 }
